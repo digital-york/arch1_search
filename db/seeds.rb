21b131ca4344fb63f2a9db84f6435a246cc198f0
Frank Feng <y.frank.feng@gmail.com>
2019-06-07 13:32:41 +0000

    comment out inrevelent code to make db:migrate working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Book.create!(title: 'grocery shopping', description: 'pickles, eggs, red onion')
-Book.create!(title: 'wash the car')
-Book.create!(title: 'register kids for school', description: 'Register Kira for Ruby Junior High and Caleb for Rails High School')
-Book.create!(title: 'check engine light', description: 'The check engine light is on in the Tacoma')
-Book.create!(title: 'dog groomers', description: 'Take Pinky and Redford to the groomers on Wednesday the 23rd')
+# Book.create!(title: 'grocery shopping', description: 'pickles, eggs, red onion')
+# Book.create!(title: 'wash the car')
+# Book.create!(title: 'register kids for school', description: 'Register Kira for Ruby Junior High and Caleb for Rails High School')
+# Book.create!(title: 'check engine light', description: 'The check engine light is on in the Tacoma')
+# Book.create!(title: 'dog groomers', description: 'Take Pinky and Redford to the groomers on Wednesday the 23rd')
