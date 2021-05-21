90786fc789b7ab6011253398f4f33da7ba580755
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2021-05-21 10:54:48 +0100

    [WIP] Refactoring initial SearchBuilderService
    [WIP]
    - it must support passing complex Solr parameters. Current @params use some defaults that most be changeable.
    - def query - requires further thinking as per what Solr query we construct - [my current experiments are not conclusive](https://docs.google.com/document/d/1_Bpz351RXlcmBCe7namgEgPFAAav1V_BFHAlFPRbHOI/edit#bookmark=id.nv96x0pxnb6c).
    - use of *fq* field conditional if values sets
    
    Refactoring questions:
    - Is this service for all different queries? Yes, but be careful how it is implemented with additional query changes comming.
    
    Inspiration comes from [Hyrax::SolrQueryBuilderService](https://github.com/samvera/hyrax/blob/main/app/services/hyrax/solr_query_builder_service.rb)

--- app/services/tnw_common/search_builder_service.rb
+++ app/services/tnw_common/search_builder_service.rb
@@ -1,22 +1,33 @@
 module TnwCommon
-    class SearchBuilderService
-        attr :search_term, :start_date, :end_date, :archival_repository
+  class SearchBuilderService
+    attr :search_term, :start_date, :end_date, :archival_repository, :parrams
 
-        def initialize(search_term:, start_date:, end_date:, archival_repository:)
-            # "Bishop Auckland" => "*Bishop* AND *Auckland*"
-            @search_term = search_term.gsub(/\b/,"*").gsub(/\s/," AND ")
-            # "" => "*"
-            @start_date = start_date.empty? ? "*" : start_date
-            @end_date = end_date.empty? ? "*" : end_date
-            @archival_repository = archival_repository
-        end
-
-        def query
-            return {
-                q: "has_model_ssim:Entry AND suggest:(#{@search_term})", 
-                fq: "entry_date_facet_ssim:[#{@start_date} TO #{end_date}]"
-            } 
-        end
-        
+    def initialize(search_term:, start_date:, end_date:, archival_repository:)
+      # "Bishop Auckland" => "*Bishop* AND *Auckland*"
+      @search_term = search_term.gsub(/\b/, "*").gsub(/\s/, " AND ")
+      # "" => "*"
+      @start_date = start_date.empty? ? "*" : start_date
+      @end_date = end_date.empty? ? "*" : end_date
+      @archival_repository = archival_repository
+      @params = {
+        q: nil,
+        fl: "id",
+        rows: 0,
+        sort: "",
+        start: 0,
+        facet: false,
+        "facet.limit": nil,
+        "facet.sort": nil,
+        "facet.field": nil, # supply a string or an array
+        fq: nil # supply a string or an array
+      }
     end
+
+    def query
+      @params.merge({
+        q: "has_model_ssim:Entry AND suggest:(#{@search_term})",
+        fq: "entry_date_facet_ssim:[#{@start_date} TO #{end_date}]"
+      })
+    end
+  end
 end
