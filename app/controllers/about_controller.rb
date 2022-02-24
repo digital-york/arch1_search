c63016dc1a08ebdbd04b29f009f6163227b1f440
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2022-02-24 12:59:50 +0000

    Create technology page
    - Move old about page content to new technology page
    - Add page route
    - Run code formater

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,19 +1,22 @@
 # added (ja)
 class AboutController < ApplicationController
-  layout "simple_layout"
+  layout 'simple_layout'
 
-  def index
-  end
+  def index; end
 
   def registers
-    render template: "about/registers"
+    render template: 'about/registers'
   end
 
   def further_resources
-    render template: "about/further_resources"
+    render template: 'about/further_resources'
   end
 
   def search_help
-    render template: "about/search_help"
+    render template: 'about/search_help'
+  end
+
+  def technology
+    render template: 'about/technology'
   end
 end
